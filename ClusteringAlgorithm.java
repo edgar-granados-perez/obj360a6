10b1207cfd405b7728519c0cb9d8ba0033d0f5cb
Edgar Granados <edgar.granados.perez@gmail.com>
2022-05-02 22:02:36 -0500

    solved

--- ClusteringAlgorithm.java
+++ ClusteringAlgorithm.java
@@ -1,6 +1,59 @@
-public class ClusteringAlgorithm {
-    Corpus corpus;
-    int k;
-    int maxIteration;
-    int n;
+import java.util.LinkedList;
+
+class ClusteringAlgorithm {
+  private LinkedList<Integer>[] cluster;
+  private int[] centroidList;
+
+  public ClusteringAlgorithm(int numCluster, Corpus corpus, int maxIteration){
+     centroidList = new int[numCluster];
+     cluster = new LinkedList[numCluster];
+
+    for(int i = 0; i < numCluster; i++){
+      centroidList[i] = i;
+      cluster[i] = new LinkedList<Integer>();
+    }
+
+    while(maxIteration != 0){
+      for(int i = 0; i < numCluster; i++){
+        cluster[i].clear();
+      }
+
+      for(int x = 0; x < corpus.size(); x++){
+        double highSim = -1.0;
+        int closeSim = -1;
+
+        for(int s = 0; s < numCluster; s++){
+          if(corpus.similarity(centroidList[s], x) > highSim){
+            highSim = corpus.similarity(centroidList[s], x);
+            closeSim = s;
+          }
+        }
+
+        cluster[closeSim].add(x);
+      }
+
+      for(int iter = 0; iter < numCluster; iter++){
+        double mCentroid = 0;
+        int index = -1;
+
+        for(int x = 0; x < cluster[iter].size(); x++){
+          double total = 0;
+
+          for(int y = 0; y < cluster[iter].size(); y++){
+            total = total + corpus.similarity(cluster[iter].get(x), cluster[iter].get(y));
+            if(total > mCentroid){
+              index = cluster[iter].get(x);
+              mCentroid = total;
+            }
+          }
+        }
+        centroidList[iter] = index;
+      }
+      maxIteration--;
+    }
+  }
+
+  public LinkedList<Integer> getCluster(int i){
+    return cluster[i];
+  }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ class Main {
     public static void main(String[] args) {
         Corpus corpus = new Corpus("documents/");
         System.out.println(corpus);
+
+        int numCluster = 3;
+        int maxIteration = 6;
+        ClusteringAlgorithm clustering = new ClusteringAlgorithm(numCluster, corpus, maxIteration);
+        for(int i = 0; i < numCluster; i++) {
+            System.out.println("cluster " + i + ": " + clustering.getCluster(i));
+        }
     }
 }
 
